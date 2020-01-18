be09f4b132f1bc1bb85d11b175629ca2283d6d11
Eduardo FM <eduardomarcos_@outlook.com>
2020-01-18 11:23:45 -0200

    add Calculate.js

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,6 +1,6 @@
 const socketio = require("socket.io");
 const transArray = require("./utils/transArray");
-const calculate = require("./calculate.js");
+const calculate = require("./utils/Calculate.js");
 
 const connections = [];
 exports.setupWebsocket = server => {
@@ -24,31 +24,6 @@ exports.setupWebsocket = server => {
     });
   });
 };
-
-function deg2rad(deg) {
-  return deg * (Math.PI / 180);
-}
-
-function getDistanceFromLatLonInKm(centerCoordinates, pointCoordinates) {
-  const radius = 6371;
-
-  const { latitude: lat1, longitude: lon1 } = centerCoordinates;
-  const { latitude: lat2, longitude: lon2 } = pointCoordinates;
-
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  const center = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = radius * center;
-
-  return distance;
-}
 
 exports.findConnections = (coordinates, techs) => {
   return connections.filter(connection => {
